73d5016a74e5bc1abacf0d15abc095e5879c3897
deangalaksin <kadirbirci2003@gmail.com>
2025-08-11 22:07:36 +0300

    Transition to Windows MATLAB

--- MATLAB/Model_Init.m
+++ MATLAB/Model_Init.m
@@ -8,15 +8,23 @@ l2 = 0.3;
 
 %% BufferTest
 
-J = 0.1;
+J = 0.2;
 B = 1e-5;
 
-L = 56e-5;
+L = 56e-3;
 R = 0.53;
 
 
 Kt = 0.08;
 Kv = 0.03
 
-meanVal = 0.0;
-varianceVal = 1; 
+meanVal = 1;
+varianceVal = 0%0.001;%1; 
+
+alpha = 0.95
+var_alpha = 0.995;
+
+safety_const = 4;
+stepSize = 1/1000;
+
+seed = 0012312;
